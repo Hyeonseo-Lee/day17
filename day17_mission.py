3f4a0f9d48d1d84bf8500d80f712437e2a33fd06
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-08 00:32:43 +0900

    7-1

--- day17_mission.py
+++ day17_mission.py
@@ -1,52 +1,73 @@
-import random
+def is_queue_full() :
+	global SIZE, queue, front, rear
+	if (rear != SIZE-1) :
+		return False
+	elif (rear == SIZE -1) and (front == -1) :
+		return True
+	else :
+		for i in range(front+1, SIZE) :
+			queue[i-1] = queue[i]
+			queue[i] = None
+		front -= 1
+		rear -= 1
+		return False
 
-class TreeNode() :
-    def __init__ (self) :
-        self.left = None
-        self.data = None
-        self.right = None
+def is_queue_empty() :
+    global SIZE, queue, front, rear
+    if (front == rear) :
+        return True
+    else :
+        return False
 
+def enQueue(data) :
+    global SIZE, queue, front, rear
+    if (is_queue_full()) :
+        print("큐가 꽉 찼습니다.")
+        return
+    rear += 1
+    queue[rear] = data
 
-memory = []
-Root = None
-overlaparr = ['레쓰비캔커피', '레쓰비캔커피', '레쓰비캔커피', '도시락', '도시락',
-              '삼각김밥', '레쓰비캔커피', '도시락', '코카콜라', '삼다수', '레쓰비캔커피',
-              '레쓰비캔커피', '레쓰비캔커피', '츄파츕스', '츄파츕스', '레쓰비캔커피',
-              '코카콜라', '츄파츕스', '삼각김밥', '코카콜라']
+def deQueue() :
+    global SIZE, queue, front, rear
+    if (is_queue_empty()) :
+        # print("큐가 비었습니다.")
+        return None
+    # front == 0
+    data = queue[front]
+    queue[front] = None
+    return data
 
-
-node = TreeNode()
-node.data = overlaparr[0]
-Root = node
-memory.append(node.data)
-
-for stuff in overlaparr[1:] :
-
-    node = TreeNode()
-    node.data = stuff
-    current = Root
-    while True :
-        if stuff < current.data :
-            if current.left == None :
-                current.left = node
-                memory.append(node.data)
-                break
-            current = current.left
-        elif stuff > current.data :
-            if current.right == None :
-                current.right = node
-                memory.append(node.data)
-                break
-            current = current.right
-        else:
-            break
-
-    #memory.append(node.data)
-
-print("오늘 판매된 물건(중복O) --> ", overlaparr)
-print("이진 탐색 트리 구성 완료!")
-print("오늘 판매된 종류(중복X)-->", memory)
+def peek() :
+    global SIZE, queue, front, rear
+    if (is_queue_empty()) :
+        print("큐가 비었습니다.")
+        return None
+    return queue[front+1]
 
 
+SIZE = int(input("대기줄의 크기를 입력하세요 ==> "))
+# queue = [ None for _ in range(SIZE) ]
+queue = ["뷔", "정국", "지민", "진", "슈가"]
+front = rear = -1
+
+if __name__ == "__main__" :
 
 
+
+        # if not is_queue_full():
+        #     for i in range(SIZE):
+        #         data = input("입력할 데이터 ==> ")
+        #         enQueue(data)
+        #         print("대기줄 상태 : ", queue)
+        #
+            print("대기줄 상태 : ", queue)
+            for s in range(5):
+                #data = deQueue()
+                a= queue[0]
+                print(f'{a}님 식당에 들어감')
+                for i in range(1, 5):
+                    queue[i-1]=queue[i]
+                    queue[i]= None
+                print("대기줄 상태 : ", queue)
+
+            print("식당 영업 종료!")
