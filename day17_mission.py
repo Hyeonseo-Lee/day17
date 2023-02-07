d3b74d5e9717716a298df9f49abf82a7f57a30b5
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-07 23:38:47 +0900

    7-2

--- day17_mission.py
+++ day17_mission.py
@@ -1,18 +1,12 @@
-def is_queue_full() :
-	global SIZE, queue, front, rear
-	if (rear != SIZE-1) :
-		return False
-	elif (rear == SIZE -1) and (front == -1) :
-		return True
-	else :
-		for i in range(front+1, SIZE) :
-			queue[i-1] = queue[i]
-			queue[i] = None
-		front -= 1
-		rear -= 1
-		return False
 
-def is_queue_empty() :
+def isQueueFull() :
+    global SIZE, queue, front, rear
+    if (rear == SIZE-1) :
+        return True
+    else :
+        return False
+
+def isQueueEmpty() :
     global SIZE, queue, front, rear
     if (front == rear) :
         return True
@@ -21,7 +15,7 @@ def is_queue_empty() :
 
 def enQueue(data) :
     global SIZE, queue, front, rear
-    if (is_queue_full()) :
+    if (isQueueFull()) :
         print("큐가 꽉 찼습니다.")
         return
     rear += 1
@@ -29,53 +23,60 @@ def enQueue(data) :
 
 def deQueue() :
     global SIZE, queue, front, rear
-    if (is_queue_empty()) :
-        # print("큐가 비었습니다.")
+    if (isQueueEmpty()) :
+        print("큐가 비었습니다.")
         return None
-    # front == 0
+    front += 1
     data = queue[front]
     queue[front] = None
     return data
 
 def peek() :
     global SIZE, queue, front, rear
-    if (is_queue_empty()) :
+    if (isQueueEmpty()) :
         print("큐가 비었습니다.")
         return None
     return queue[front+1]
 
+def call(str):
+    global SIZE, queue, front, rear, time
+    if str == "사용":
+        enQueue("('사용', 9)")
+        time += 9
+    elif str == "고장":
+        enQueue("('고장', 3)")
+        time += 3
+    elif str == "환불":
+        enQueue("('환불', 4)")
+        time += 4
+    else:
+        time += 0
 
-SIZE = int(input("대기줄의 크기를 입력하세요 ==> "))
-# queue = [ None for _ in range(SIZE) ]
-queue = ["뷔", "정국", "지민", "진", "슈가"]
-front = rear = -1
+def printqueue():
+    global SIZE, queue, front, rear, time
+    if not isQueueFull():
+        print(f'귀하의 대기 예상시간은 {time}분입니다.')
+        print('현재 대기 콜 -->', queue)
+    else:
+        print('최종 대기 콜 -->', queue)
 
+SIZE = 6
+queue = [ None for _ in range(SIZE) ]
+front = rear = 0
+time = 0
+## 메인 코드 부분 ##
 if __name__ == "__main__" :
 
+    printqueue()
+    call("사용")
+    printqueue()
+    call("고장")
+    printqueue()
+    call("환불")
+    printqueue()
+    call("환불")
+    printqueue()
+    call("고장")
+    printqueue()
 
-
-        # if not is_queue_full():
-        #     for i in range(SIZE):
-        #         data = input("입력할 데이터 ==> ")
-        #         enQueue(data)
-        #         print("대기줄 상태 : ", queue)
-        #
-            print("대기줄 상태 : ", queue)
-            for s in range(5):
-                data = deQueue()
-                a= queue[0]
-                print(f'{a}님 식당에 들어감')
-                for i in range(1, 5):
-                    queue[i-1]=queue[i]
-                    queue[i]= None
-                print("대기줄 상태 : ", queue)
-
-            print("식당 영업 종료!")
-
-
-
-
-
-
-
-
+    print("프로그램 종료!")
